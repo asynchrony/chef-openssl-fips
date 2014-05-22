7765e95465fa85c8b6ca46754decb56d571c6c09
Alex <sasha@merqlove.ru>
2014-05-23 02:01:44 +0400

    Update default.rb

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,11 +1,6 @@
 ## Cookbook Name:: openssl-fips
 ## Attributes:: default
 
-fips_version = '2.0.5'
-default['openssl_fips']['fips']['version']  = fips_version
-default['openssl_fips']['fips']['url']      = "https://www.openssl.org/source/openssl-fips-#{fips_version}.tar.gz"
-default['openssl_fips']['fips']['checksum'] = '54ad8102f73488e9f34d3143b55866a147582810'
-
 openssl_version = '1.0.1g'
 default['openssl_fips']['openssl']['version'] = openssl_version
 default['openssl_fips']['openssl']['prefix']  = "/opt/openssl-#{openssl_version}"
