50f5d115ee4dd0b745da73e3d2eac841606e1bea
Alex <sasha@merqlove.ru>
2014-05-23 02:02:38 +0400

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,8 @@
-name             'openssl-fips'
-maintainer       'Matt Campbell'
-maintainer_email 'matthew.campbell@asynchrony.com'
+name             'openssl'
+maintainer       'Alexander Merkulov'
+maintainer_email 'api@mrcr.ru'
 license          'Apache 2.0'
-description      'Installs/Configures OpenSSL from source with FIPS 140-2 mode enabled'
+description      'Installs/Configures OpenSSL from source'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.0.1'
 
