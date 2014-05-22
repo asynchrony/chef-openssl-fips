3e878f8b4d344cffaf0c1114353f60704be21599
Alex <sasha@merqlove.ru>
2014-05-23 02:07:30 +0400

    Update default.rb

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,9 +1,9 @@
-## Cookbook Name:: openssl-fips
+## Cookbook Name:: openssl-source
 ## Attributes:: default
 
 openssl_version = '1.0.1g'
-default['openssl_fips']['openssl']['version'] = openssl_version
-default['openssl_fips']['openssl']['prefix']  = "/opt/openssl-#{openssl_version}"
-default['openssl_fips']['openssl']['url'] = "https://www.openssl.org/source/openssl-#{openssl_version}.tar.gz"
-default['openssl_fips']['openssl']['checksum'] = 'b28b3bcb1dc3ee7b55024c9f795be60eb3183e3c'
-default['openssl_fips']['openssl']['configure_flags'] = %W[ shared ]
+default['openssl_source']['openssl']['version'] = openssl_version
+default['openssl_source']['openssl']['prefix']  = "/opt/openssl-#{openssl_version}"
+default['openssl_source']['openssl']['url'] = "https://www.openssl.org/source/openssl-#{openssl_version}.tar.gz"
+default['openssl_source']['openssl']['checksum'] = 'b28b3bcb1dc3ee7b55024c9f795be60eb3183e3c'
+default['openssl_source']['openssl']['configure_flags'] = %W[ shared ]
